63c46c9a2b1333d09663e0f5963c1b60a8a13b09
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2022-03-14 09:26:21 +0100

    small change to barber slug page

--- pages/about/[slug].tsx
+++ pages/about/[slug].tsx
@@ -18,7 +18,8 @@ function SingleAuthor({ author }: Props) {
       </Head>
       <Header />
       <main>
-        {/* Sub-Header */}
+        {/* Sub-Header
+        Lägg till fält i sanity för att kunna ändra header bild */}
         <div className="top relative h-64 w-full overflow-hidden bg-blue-600">
           <img
             src="https://images.unsplash.com/photo-1497384401032-2182d2687715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
@@ -32,6 +33,13 @@ function SingleAuthor({ author }: Props) {
             />
             <h1 className="text-2xl font-semibold">{author.name}</h1>
             {/* Lägg instagram och facebook ikoner*/}
+            <svg
+              className='h-6 w-6 fill-current text-blue-500'
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+            </svg>
             <h4 className="text-sm font-semibold">Joined Since '19</h4>
           </div>
         </div>
